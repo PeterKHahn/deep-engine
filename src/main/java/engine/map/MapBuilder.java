34acb150c0d8cdb798f0a6665d884b9cc9e01529
Peter Hahn <peterkunho@gmail.com>
2018-12-20 16:53:42 -0500

    Creating map building mechanics for initial state

---
+++ src/main/java/engine/map/MapBuilder.java
@@ -0,0 +1,27 @@
+package engine.map;
+
+
+/**
+ * Tool for Building the Initial State Maps that a game can take on.
+ */
+public class MapBuilder {
+
+
+    private double width;
+    private double height;
+
+    public MapBuilder(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public void insertStaticEntity() {
+
+    }
+
+    public void insertDynamicEntity() {
+
+    }
+
+
+}
